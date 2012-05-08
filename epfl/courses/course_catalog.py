6286b9df50946734b2f71560f7af3352f3b601fd
Stefan Bucur <stefan.bucur@gmail.com>
2012-05-09 01:39:02 +0200

    Checkpoint. Brushed up some style on the catalog page.

--- epfl/courses/course_catalog.py
+++ epfl/courses/course_catalog.py
@@ -7,22 +7,45 @@ import jinja2
 import os
 import webapp2
 
-from epfl.courses import model
+from google.appengine.ext import db
+
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+language_mapping = {
+  "en": "English",
+  "fr": "French"
+}
+
+
+class Course(db.Model):
+  name = db.StringProperty(required=True)
+  language = db.StringProperty(required=True, choices=set(["en", "fr"]))
+  teachers = db.StringListProperty()
+  studies = db.StringListProperty()
+  orientations = db.StringListProperty()
+  urls = db.StringListProperty()
+
+
 class CatalogPage(webapp2.RequestHandler):
   def get(self):
-    courses_query = model.Course.all()
-    
+    courses_query = Course.all()
     courses = courses_query.run()
     
     template = jinja_environment.get_template('catalog.html')
     self.response.out.write(template.render(courses=courses))
 
-class CoursePage(webapp2.RequestHandler):
-  def get(self):
-    pass
 
-app = webapp2.WSGIApplication([('/', CatalogPage)])
+class CoursePage(webapp2.RequestHandler):
+  def get(self, course_key):
+    course = db.get(course_key)
+    course._language = language_mapping[course.language]
+    
+    template = jinja_environment.get_template('course.html')
+    self.response.out.write(template.render(course=course))
+
+
+app = webapp2.WSGIApplication([
+    (r'/', CatalogPage),
+    (r'/c/([0-9a-zA-z\-]+)', CoursePage)], debug=True)
