77d3c1aa1cd6de5f6b788e675b43e8eafa8a5cba
Stefan Bucur <stefan.bucur@gmail.com>
2012-05-15 19:37:46 +0200

    First crawl complete.

---
+++ crawl/crawl_isa_epfl.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""Crawl EPFL IS-Academia's course books."""
+
+__author__ = "stefan.bucur@epfl.ch (Stefan Bucur)"
+
+import json
+import logging
+import os
+import urllib
+
+DATA_FILE = os.path.join(os.path.dirname(__file__),
+                         'course_info.json')
+
+COURSE_BOOK_BASE_URL = "http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.txt\
+?ww_i_reportModel=83782576&ww_i_reportModelXsl=83782665\
+&ww_x_MAT=%d&ww_x_PERIODE_ACAD=%d&ww_x_SECTION=%d&ww_x_BLOC=%d"
+
+COURSE_BOOK_BASE_URL_ALT = "http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.txt\
+?ww_i_reportModel=5059249&ww_i_reportModelXsl=5059315\
+&ww_x_MAT=%d&ww_x_PROGRAMME=%d"
+
+
+def main():
+  logging.basicConfig(level=logging.INFO)
+  
+  course_data = None
+  with open(DATA_FILE, "r") as f:
+    course_data = json.load(f, encoding="utf-8")
+    
+  if not course_data:
+    logging.error("No course data found")
+    return
+  
+  counters = {}
+  
+  for course in course_data:
+    counters[course["id"]] = counters.get(course["id"], 0) + 1
+    
+    logging.info("Processing '%s'" % course["title"])
+    print "Before:", course["urls"]
+    
+    url_params = dict([pair.split("=") 
+                  for pair in course["urls"].split("?")[1].split("&")])
+    
+    course_book_url = None
+    if url_params['ww_i_niveau']:
+      course_book_url = COURSE_BOOK_BASE_URL % (
+        int(url_params['ww_i_matiere']),
+        int(url_params['ww_x_anneeacad']),
+        int(url_params['ww_i_section']),
+        int(url_params['ww_i_niveau'])
+      )
+    else:
+      course_book_url = COURSE_BOOK_BASE_URL_ALT % (
+        int(url_params['ww_i_matiere']),
+        int(url_params['ww_i_section'])
+      )
+    
+    url_handle = urllib.urlopen(course_book_url)
+    url_data = url_handle.read()
+    with open(os.path.join(os.path.dirname(__file__),
+                           "%s-%d.xml" % (course["id"], counters[course["id"]])), "w") as f:
+      f.write(url_data)
+
+    url_handle.close()
+
+
+if __name__ == "__main__":
+  main()
