0f76418aa43c694f706343163318d2880e639622
Stefan Bucur <stefan.bucur@epfl.ch>
2012-09-05 18:21:56 +0200

    Fetched the French descriptions of courses.

--- crawl/2012-2013/parse_study_plans.py
+++ crawl/2012-2013/parse_study_plans.py
@@ -32,6 +32,7 @@ this_dir = os.path.dirname(__file__)
 study_plans_path = os.path.join(this_dir, "study_plans.json")
 course_list_path = os.path.join(this_dir, "course_list.json")
 course_desc_path = os.path.join(this_dir, "course_desc.json")
+course_desc_path_fr = os.path.join(this_dir, "course_desc_fr.json")
 
 
 def _ProcessStudyPlan(plan_id, plan_name, plan_soup, id_trim_size=3):
@@ -110,7 +111,7 @@ def _FetchCoursesInSection(plan_id, section_id, section_url):
     if parsed_url.netloc != "isa.epfl.ch":
       print "-- Invalid course entry for ", course_name, "at", course_url
       continue
-    
+
     course_url_fr = re.sub(r"ww_c_langue=en", "ww_c_langue=fr", course_url)
 
     courses.append({
@@ -148,6 +149,12 @@ def ShowCourseListStatistics(courses):
 
   print "Total courses:", len(course_list)
   print "Unique course titles:", len(title_set)
+  
+
+class ParsingError(Exception):
+  """Occurs when the parser doesn't recognize the page structure."""
+  
+  pass
 
 
 class ParsedCourseDescription(object):
@@ -158,6 +165,18 @@ class ParsedCourseDescription(object):
   language_title = "Language"
   links_title = "Links"
   library_rec_title = "The library recommends"
+
+  semester_label = "Semester"
+  exam_form_label = "Exam form"
+  credits_label = "Credits"
+  hpw_label = "Hour(s) per week x 14 weeks"
+  coefficient_label = "Coefficient"
+  subjects_label = "Subject examined"
+  lecture_label = "Lecture"
+  recitation_label = "Recitation"
+  project_label = "Project"
+  labs_label = "Labs"
+  practical_label = "Practical work"
 
   # Make this a set, for quick lookup
   free_text_titles = set([
@@ -171,33 +190,33 @@ class ParsedCourseDescription(object):
     "Bibliography and material",
     "Note",
   ])
-  
+
   week_hours_re = re.compile(r"(\d+)\s+Hour\(s\) per week x (\d+) weeks",
-                                 flags=re.UNICODE)
+                             flags=re.UNICODE)
   sem_hours_re = re.compile(r"(\d+)\s+Hour\(s\) per semester",
-                                flags=re.UNICODE)
+                            flags=re.UNICODE)
   total_hours_re = re.compile(r"(\d+)\s+Hour\(s\)",
                               flags=re.UNICODE)
 
-  def __init__(self, url, soup):
+  def __init__(self, soup, err_on_unknown=True):
     self._soup = soup
+    self._err_on_unknown = err_on_unknown
 
-    self.url = url
     self.title = None
     self.instructors = []
     self.language = None
-    
+
     self.semester = None
     self.credits = None
     self.coefficient = None
     self.exam_form = None
-    
+
     self.lecture_hours = None
     self.recitation_hours = None
     self.project_hours = None
     self.lab_hours = None
     self.practical_hours = None
-    
+
     self.free_text_desc = []
     self.links = []
     self.library_rec = None
@@ -255,7 +274,7 @@ class ParsedCourseDescription(object):
         contents = "".join(entry.contents)
 
     self.free_text_desc.append((title, contents))
-    
+
   def _ParseMainContent(self):
     main_content = self._soup.find(id="content")
 
@@ -277,102 +296,155 @@ class ParsedCourseDescription(object):
       elif subsection_title in self.library_rec_title:
         self._ParseLibraryRec(subsection)
       else:
-        self.unknown_subsections.add(subsection.string.strip())
-        
+        self.unknown_subsections.add(subsection_title)
+        if self._err_on_unknown:
+          raise ParsingError("Unrecognized section name '%s'" % subsection_title)
+
   @classmethod
   def _ParseCourseHours(cls, value):
     match = cls.week_hours_re.match(value)
-    if match:      
+    if match:
       return {
         "week_hours": int(match.group(1)),
         "weeks": int(match.group(2)),
       }
-    
+
     match = cls.sem_hours_re.match(value)
     if match:
       return {
-        "total_hours": int(match.group(1)),      
+        "total_hours": int(match.group(1)),
       }
-      
+
     match = cls.total_hours_re.match(value)
     if match:
       return {
         "total_hours": int(match.group(1)),
       }
-    
-    print "** Invalid course hours '%s'" % value
-    exit(1)
-    return None
-        
+      
+    raise ParsingError("Invalid course hours: '%s'" % value)
+
   def _ParseSideBar(self):
     side_bar = self._soup.find(class_="right-col")
     first_plan = side_bar.find("ul").find("ul")
-    
+
     if not first_plan:
       return
-    
+
     for item in first_plan.find_all("li"):
       if not item.strong.string:
         continue
-      
+
       item_label = item.strong.string.strip()
       item_value = " ".join([s for s in item.stripped_strings
                              if s != item_label])
-      
+
       if not item_value:
         print "** Empty item value for '%s'" % item_label
         continue
-      
+
       print item_label, "=", item_value
-      
+
       # TODO(bucur): Move string constants to class definition, to support
       # French in the future.
-      if item_label == "Semester":
+      if item_label == self.semester_label:
         self.semester = item_value
-      elif item_label == "Exam form":
+      elif item_label == self.exam_form_label:
         self.exam_form = item_value
-      elif (item_label == "Credits" 
-            or item_label == "Hour(s) per week x 14 weeks"):
+      elif (item_label == self.credits_label
+            or item_label == self.hpw_label):
         self.credits = int(item_value)
-      elif item_label == "Coefficient":
+      elif item_label == self.coefficient_label:
         self.coefficient = float(item_value)
-      elif item_label == "Subject examined":
+      elif item_label == self.subjects_label:
         pass # Ignored
-      elif item_label == "Lecture":
+      elif item_label == self.lecture_label:
         self.lecture_hours = self._ParseCourseHours(item_value)
-      elif item_label == "Recitation":
+      elif item_label == self.recitation_label:
         self.recitation_hours = self._ParseCourseHours(item_value)
-      elif item_label == "Project":
+      elif item_label == self.project_label:
         self.project_hours = self._ParseCourseHours(item_value)
-      elif item_label == "Labs":
+      elif item_label == self.labs_label:
         self.lab_hours = self._ParseCourseHours(item_value)
-      elif item_label == "Practical work":
+      elif item_label == self.practical_label:
         self.practical_hours = self._ParseCourseHours(item_value)
       else:
         self.unknown_labels.add(item_label)
+        if self._err_on_unknown:
+          raise ParsingError("Unrecognized item label '%s'" % item_label)
 
   def ParseDescription(self):
     """Populate all course fields."""
 
     self._ParseMainContent()
-    self._ParseSideBar()    
-
-
-@caching.CachedJSON(course_desc_path)
-def FetchCourseDescriptions(courses):
-  all_unknown_subsections = set()
+    self._ParseSideBar()
+    
+    
+class ParsedCourseDescriptionFrench(ParsedCourseDescription):
+  """Parse French course descriptions."""
   
+  lecturer_title = u"Enseignant"
+  language_title = u"Langue"
+  links_title = u"Liens"
+  library_rec_title = u"La bibliothèque vous propose"
+
+  semester_label = u"Semestre"
+  exam_form_label = u"Forme de l'examen"
+  credits_label = u"Crédits"
+  hpw_label = u"Heure(s) hebdo x 14 semaines"
+  coefficient_label = u"Coefficient"
+  subjects_label = u"Matière examinée"
+  lecture_label = u"Cours"
+  recitation_label = u"Exercices"
+  project_label = u"Projet"
+  labs_label = u"Labo"
+  practical_label = u"TP"
+
+  # Make this a set, for quick lookup
+  free_text_titles = set([
+    u"Préparation pour",
+    u"Objectifs d'apprentissage",
+    u"Contenu",
+    u"Forme d'enseignement",
+    u"Forme du contrôle",
+    u"Mots clés",
+    u"Prérequis",
+    u"Bibliographie et matériel",
+    u"Remarque",
+  ])
+
+  week_hours_re = re.compile(r"(\d+)\s+Heure(s) hebdo x (\d+) semaines",
+                             flags=re.UNICODE)
+  sem_hours_re = re.compile(r"(\d+)\s+Hour\(s\) per semester",
+                            flags=re.UNICODE)
+  total_hours_re = re.compile(r"(\d+)\s+Heure\(s\)",
+                              flags=re.UNICODE)
+
+
+def _FetchCourseDescriptions(courses, use_french=False):
+  all_unknown_subsections = set()
+
   descriptions = []
 
   for course in courses["courses"]:
-    course_desc_html = caching.CachedURLGet(course["url"])
-    course_desc_html_fr = caching.CachedURLGet(course["url_fr"])
+    if use_french:
+      course_desc_html = caching.CachedURLGet(course["url_fr"])
+    else:
+      course_desc_html = caching.CachedURLGet(course["url"])
     
+
     soup = BeautifulSoup(course_desc_html)
 
-    course_desc = ParsedCourseDescription(course["url"], soup)
-    course_desc.ParseDescription()
-    
+    if use_french:
+      course_desc = ParsedCourseDescriptionFrench(soup)
+    else:
+      course_desc = ParsedCourseDescription(soup)
+      
+    try:
+      course_desc.ParseDescription()
+    except ParsingError as e:
+      print "** Parsing error:", e.message
+      raise
+
     descriptions.append({
       "title": course_desc.title,
       "instructors": [{ "name": i[0], "url": i[1] }
@@ -410,10 +482,20 @@ def FetchCourseDescriptions(courses):
 
   if all_unknown_subsections:
     print "** All unknown subsections:", ", ".join(all_unknown_subsections)
-    
+
   return {
     "descriptions": descriptions,
   }
+  
+  
+@caching.CachedJSON(course_desc_path)
+def FetchCourseDescriptionsEnglish(courses):
+  return _FetchCourseDescriptions(courses, use_french=False)
+
+
+@caching.CachedJSON(course_desc_path_fr)
+def FetchCourseDescriptionsFrench(courses):
+  return _FetchCourseDescriptions(courses, use_french=True)
 
 
 def Main():
@@ -422,7 +504,9 @@ def Main():
   study_plans = FetchStudyPlans()
   courses = FetchCourseList(study_plans)
   ShowCourseListStatistics(courses)
-  descriptions = FetchCourseDescriptions(courses)
+  
+  FetchCourseDescriptionsEnglish(courses)
+  FetchCourseDescriptionsFrench(courses)
 
 
 if __name__ == "__main__":
